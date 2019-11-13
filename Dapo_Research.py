60628e8e501ef74ba57dcf547ef4085507269fcb
akashgokul <akashgokul@berkeley.edu>
2019-11-12 21:52:41 -0800

    Testing other models

--- Dapo_Research.py
+++ Dapo_Research.py
@@ -70,7 +70,7 @@ if torch.cuda.device_count() > 1:
 
 model = model.to(device)
 
-max_epochs = 5
+max_epochs = 10
 
 loss_function = nn.CrossEntropyLoss()
 optimizer = optim.Adadelta(model.parameters())
