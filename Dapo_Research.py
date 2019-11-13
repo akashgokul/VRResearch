16da4f01e266bd7513378982162daf2a7b465617
akashgokul <akashgokul@berkeley.edu>
2019-11-12 19:47:50 -0800

    Deleting print statements

--- Dapo_Research.py
+++ Dapo_Research.py
@@ -133,7 +133,6 @@ model.eval()
 with torch.set_grad_enabled(False):
   val_wrong = 0
   for i, data in enumerate(validation_generator):
-    print("Current: " + str(i) + " / " )
     # Transfer to GPU
     X, y = data[0].to(device), data[1].to(device)
      # Model computations
