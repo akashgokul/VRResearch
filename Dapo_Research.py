5b1a62afa4c4d94959b9a5042d4fca263194b6b7
akashgokul <akashgokul@berkeley.edu>
2019-11-15 13:05:16 -0800

    Trying GoogLenet

--- Dapo_Research.py
+++ Dapo_Research.py
@@ -61,7 +61,7 @@ validation_generator = data.DataLoader(validation_set, **params_v)
 
 
 
-model = models.googlenet()
+model = models.squeezenet1_0()
 
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
