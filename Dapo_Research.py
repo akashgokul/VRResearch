2128ab11d930c114eab10506ecf480438108e8b8
akashgokul <akashgokul@berkeley.edu>
2019-11-15 14:47:35 -0800

    Trying Shufflenet

--- Dapo_Research.py
+++ Dapo_Research.py
@@ -61,7 +61,7 @@ validation_generator = data.DataLoader(validation_set, **params_v)
 
 
 
-model = models.squeezenet1_1()
+model = models.squeezenet1_0()
 
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
@@ -148,6 +148,6 @@ print("Validation Accuracy: ")
 val_acc = 1 - (val_wrong / total)
 print(val_acc)
 if val_acc >= 0.7:
-    model_save_name = 'squeeze1.pt'
+    model_save_name = 'squeeze.pt'
     path = model_save_name
     torch.save(model.state_dict(), path)
