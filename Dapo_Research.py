75c84114985752e8818ab3dd230ecbdfb1eb94f7
akashgokul <akashgokul@berkeley.edu>
2019-11-15 20:15:35 -0800

    Trying Shufflenet

--- Dapo_Research.py
+++ Dapo_Research.py
@@ -61,7 +61,7 @@ validation_generator = data.DataLoader(validation_set, **params_v)
 
 
 
-model = torchvision.models.segmentation.fcn_resnet101(pretrained=False, progress=True, num_classes=2, aux_loss=None)
+model = models.segmentation.fcn_resnet101(pretrained=False, progress=True, num_classes=2, aux_loss=None)
 
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
